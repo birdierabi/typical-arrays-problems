fd1a46c13004f900957a5a7b27e9133da7b4b4f4
birdierabi <vnefremova8@gmail.com>
2020-09-30 13:35:16 +0300

    feat: add solution typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  return (!Array.isArray(array) || array.length === 0) ? 0 : Math.min(...array);
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  return (!Array.isArray(array) || array.length === 0) ? 0 : Math.max(...array);
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  } else {
+    let result = 0;
+    for (let i = 0; i < array.length; i++) {
+      result += array[i];
+    };
+    return result / array.length;
+  };
+};
